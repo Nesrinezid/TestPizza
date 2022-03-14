1005d6b79f78bdc5a3b0573f9a3ef2b3f3b25def
Nesrinezid <98804757+Nesrinezid@users.noreply.github.com>
2022-03-14 21:27:51 +0100

    J'ai supprimé removed_pizza

---
+++ CartePizza.py
@@ -0,0 +1,27 @@
+from cmath import e
+
+
+class CartePizza:
+
+    def __init__(self, pizza=[]):
+        self.pizza=pizza
+
+    # def is_empty(self):
+    # # checking the length
+    # if len(list) == 0:
+    #     # returning true as length is 0
+    #     return True
+    # # returning false as length is greater than 0
+    # return False
+
+    def nb_pizzas(self):
+     return len(self.pizza)    
+
+    def is_empty(self):
+        return len(self.pizza)==0 
+
+    def add_pizzas(self,pizza):
+        self.pizza.append(pizza)
+
+
+
